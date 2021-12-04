950e517ebb8a7b95007324bb19b332cacfc7a378
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-04 18:34:36 +0000

    Create StroageManagerProducer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/one/microstream/cdi/StorageManagerProducer.java
@@ -0,0 +1,26 @@
+package one.microstream.cdi;
+
+import one.microstream.storage.types.StorageManager;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+class StorageManagerProducer {
+
+    private static final String DEFAULT_CONFIGURATION = "miccrostream";
+
+    @Produces
+    @ApplicationScoped
+    public StorageManager getStoreManager() {
+        Config config = ConfigProvider.getConfig();
+        return config.getValue(DEFAULT_CONFIGURATION, StorageManager.class);
+    }
+
+    public void dispose(@Disposes StorageManager manager) {
+        manager.close();
+    }
+}

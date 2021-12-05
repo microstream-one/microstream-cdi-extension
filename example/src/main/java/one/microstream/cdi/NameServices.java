13c956c46001baadc46d4e93f374acbeb1f4b672
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-05 19:41:08 +0000

    Create NameServices to sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ example/src/main/java/one/microstream/cdi/NameServices.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2021 Otavio Santana
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package one.microstream.cdi;
+
+import one.microstream.storage.types.StorageManager;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Objects;
+import java.util.Set;
+
+@ApplicationScoped
+public class NameServices {
+
+    @Inject
+    private StorageManager manager;
+
+    private NameRoot nameRoot;
+
+    @PostConstruct
+    public void setUp() {
+        Object root = manager.root();
+        if (Objects.isNull(root)) {
+            this.nameRoot = new NameRoot();
+            manager.setRoot(nameRoot);
+        } else {
+            this.nameRoot = (NameRoot) root;
+        }
+    }
+
+    @UpdateRoot
+    public void add(String name) {
+        this.nameRoot.add(name);
+    }
+
+    public Set<String> getNames() {
+        return this.nameRoot.getNames();
+    }
+
+}

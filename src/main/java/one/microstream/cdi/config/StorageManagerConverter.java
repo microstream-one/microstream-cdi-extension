d2bb3494380d851594f8ec3f8afdaf64b2a0931e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-05 17:57:10 +0000

    Add a map to avoid duplicated creation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/one/microstream/cdi/config/StorageManagerConverter.java
+++ src/main/java/one/microstream/cdi/config/StorageManagerConverter.java
@@ -15,13 +15,20 @@
 
 package one.microstream.cdi.config;
 
+import one.microstream.configuration.types.Configuration;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfigurationBuilder;
 import one.microstream.storage.embedded.types.EmbeddedStorageFoundation;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+import one.microstream.storage.exceptions.StorageExceptionInitialization;
+import one.microstream.storage.types.Database;
+import one.microstream.storage.types.Databases;
 import one.microstream.storage.types.StorageManager;
 import org.eclipse.microprofile.config.spi.Converter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 /**
@@ -31,12 +38,21 @@ public class StorageManagerConverter implements Converter<StorageManager> {
 
     private static final Logger LOGGER = Logger.getLogger(StorageManagerConverter.class.getName());
 
+    private static final Map<String, StorageManager> MAP = new ConcurrentHashMap<>();
+
     @Override
     public StorageManager convert(String value) throws IllegalArgumentException, NullPointerException {
-        LOGGER.info("Loading configuration to start the class StorageManager from the key: " + value);
-        EmbeddedStorageConfigurationBuilder load = EmbeddedStorageConfiguration.load(value);
-        EmbeddedStorageFoundation<?> embeddedStorageFoundation = load.createEmbeddedStorageFoundation();
-        EmbeddedStorageManager embeddedStorageManager = embeddedStorageFoundation.createEmbeddedStorageManager();
-        return embeddedStorageManager.start();
+        if (MAP.get(value) == null) {
+            synchronized (MAP) {
+                LOGGER.info("Loading configuration to start the class StorageManager from the key: " + value);
+                EmbeddedStorageConfigurationBuilder configurationBuilder = EmbeddedStorageConfiguration.load(value);
+                EmbeddedStorageFoundation<?> embeddedStorageFoundation = configurationBuilder.createEmbeddedStorageFoundation();
+                EmbeddedStorageManager embeddedStorageManager = embeddedStorageFoundation.createEmbeddedStorageManager();
+                EmbeddedStorageManager manager = embeddedStorageManager.start();
+                MAP.put(value, manager);
+                return manager;
+            }
+        }
+        return MAP.get(value);
     }
 }

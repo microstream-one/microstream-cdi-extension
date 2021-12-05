c664f080831f69843b47f2655af33268031f2a65
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-05 19:25:02 +0000

    Create first sample of integration with StorageManager
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- example/src/main/java/one/microstream/cdi/App.java
+++ example/src/main/java/one/microstream/cdi/App.java
@@ -1,5 +1,7 @@
 package one.microstream.cdi;
 
+import one.microstream.storage.types.StorageManager;
+
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 
@@ -7,6 +9,7 @@ public class App {
 
     public static void main(String[] args) {
         try(SeContainer container = SeContainerInitializer.newInstance().initialize()){
+            StorageManager manager = container.select(StorageManager.class).get();
 
         }
     }

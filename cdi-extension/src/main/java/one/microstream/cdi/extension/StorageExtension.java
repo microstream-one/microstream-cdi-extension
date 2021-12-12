4c985cf77038f9c6158e14446c242559211cf783
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-12 21:55:59 +0000

    Create the logic behind Storage extensionl
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-extension/src/main/java/one/microstream/cdi/extension/StorageExtension.java
+++ cdi-extension/src/main/java/one/microstream/cdi/extension/StorageExtension.java
@@ -14,9 +14,34 @@
  */
 package one.microstream.cdi.extension;
 
+import one.microstream.cdi.Storage;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.WithAnnotations;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationScoped
 public class StorageExtension implements Extension {
+
+    private final Set<Class<?>> storageRoot = new HashSet<>();
+
+    public <T> void loadEntity(@Observes @WithAnnotations({Storage.class}) final ProcessAnnotatedType<T> target) {
+        AnnotatedType<T> annotatedType = target.getAnnotatedType();
+        if (annotatedType.isAnnotationPresent(Storage.class)) {
+            Class<T> javaClass = target.getAnnotatedType().getJavaClass();
+            storageRoot.add(javaClass);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "StorageExtension{" +
+                "storageRoot=" + storageRoot +
+                '}';
+    }
 }

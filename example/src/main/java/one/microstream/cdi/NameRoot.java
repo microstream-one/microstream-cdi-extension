dd6e690ace97fcb949eae913498413540006115c
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-15 10:54:45 +0000

    Use NameRoot annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- example/src/main/java/one/microstream/cdi/NameRoot.java
+++ example/src/main/java/one/microstream/cdi/NameRoot.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-@Vetoed
+@Storage
 public class NameRoot {
 
     private List<String> names = new ArrayList<>();

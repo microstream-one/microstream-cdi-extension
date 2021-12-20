d00fc9be51a427aabd536ea30ca10853f2cfd95b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-12-20 16:18:55 +0000

    Increase tet with read though
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-extension/src/test/java/one/microstream/cdi/cache/CachePropertiesTest.java
+++ cdi-extension/src/test/java/one/microstream/cdi/cache/CachePropertiesTest.java
@@ -38,12 +38,46 @@ class CachePropertiesTest {
     }
 
     @Test
-    @DisplayName("Should get values from Config")
+    @DisplayName("Should get storeByValue from Eclipse MicroProfile Config")
     public void shouldReturnStoreByValueConfig() {
         System.setProperty(CacheProperties.CACHE_STORE_VALUE.get(), "false");
         Assertions.assertFalse(CacheProperties.getStoreByValue(config));
         System.setProperty(CacheProperties.CACHE_STORE_VALUE.get(), "true");
         Assertions.assertTrue(CacheProperties.getStoreByValue(config));
         System.clearProperty(CacheProperties.CACHE_STORE_VALUE.get());
+    }
+
+    @Test
+    @DisplayName("Should get the default value in the writeThrough")
+    public void shouldReturnWriteThrough() {
+        boolean storeByValue = CacheProperties.getWriteThrough(config);
+        Assertions.assertFalse(storeByValue);
+    }
+
+    @Test
+    @DisplayName("Should get writeThrough from Eclipse MicroProfile Config")
+    public void shouldReturnSWriteThroughConfig() {
+        System.setProperty(CacheProperties.CACHE_WRITE_THROUGH.get(), "false");
+        Assertions.assertFalse(CacheProperties.getStoreByValue(config));
+        System.setProperty(CacheProperties.CACHE_WRITE_THROUGH.get(), "true");
+        Assertions.assertTrue(CacheProperties.getStoreByValue(config));
+        System.clearProperty(CacheProperties.CACHE_WRITE_THROUGH.get());
+    }
+
+    @Test
+    @DisplayName("Should get the default value in the readThrough")
+    public void shouldReturnReadThrough() {
+        boolean storeByValue = CacheProperties.getReadThrough(config);
+        Assertions.assertFalse(storeByValue);
+    }
+
+    @Test
+    @DisplayName("Should get readThrough from Eclipse MicroProfile Config")
+    public void shouldReturnReadThroughConfig() {
+        System.setProperty(CacheProperties.CACHE_READ_THROUGH.get(), "false");
+        Assertions.assertFalse(CacheProperties.getStoreByValue(config));
+        System.setProperty(CacheProperties.CACHE_READ_THROUGH.get(), "true");
+        Assertions.assertTrue(CacheProperties.getStoreByValue(config));
+        System.clearProperty(CacheProperties.CACHE_READ_THROUGH.get());
     }
 }

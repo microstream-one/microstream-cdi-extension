df8e5bb10b509e7401eb1a22f0e3c2bd4866d72a
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-09 21:55:35 +0000

    Update the annotation to use Store
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-extension/src/main/java/one/microstream/cdi/extension/StoreInterceptor.java
+++ cdi-extension/src/main/java/one/microstream/cdi/extension/StoreInterceptor.java
@@ -13,8 +13,37 @@
  */
 package one.microstream.cdi.extension;
 
+import one.microstream.cdi.Store;
+import one.microstream.storage.types.StorageManager;
+
+import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.util.logging.Logger;
 
 @ApplicationScoped
+@Store
+@Interceptor
+@Priority(Interceptor.Priority.APPLICATION)
 class StoreInterceptor {
+
+    private static final Logger LOGGER = Logger.getLogger(StoreInterceptor.class.getName());
+
+    @Inject
+    private StorageManager manager;
+
+    @AroundInvoke
+    public Object auditMethod(InvocationContext ctx) throws Exception {
+
+        Object result = ctx.proceed();
+        long storeRoot = manager.storeRoot();
+        String message = String.format("Saving the state of root at %s in the method %s, storeRoot id %d",
+                ctx.getTarget().getClass(), ctx.getMethod().getName(), storeRoot);
+
+        LOGGER.info(message);
+        return result;
+    }
 }

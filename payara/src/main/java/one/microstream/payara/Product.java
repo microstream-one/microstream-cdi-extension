6819b6c01506935dfcec24086cf190ca1555db36
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-13 06:27:47 +0000

    Add Product entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ payara/src/main/java/one/microstream/payara/Product.java
@@ -0,0 +1,75 @@
+/*
+ *    Copyright 2021 Otavio Santana
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package one.microstream.payara;
+
+import javax.json.bind.annotation.JsonbCreator;
+import javax.json.bind.annotation.JsonbProperty;
+import java.util.Objects;
+
+public class Product {
+
+    private final String name;
+    private final String description;
+    private final int rating;
+
+    @JsonbCreator
+    public Product(@JsonbProperty("name") String name,
+                   @JsonbProperty("description") String description,
+                   @JsonbProperty("rating") int rating) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return rating == product.rating
+                && Objects.equals(name, product.name)
+                && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, rating);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}

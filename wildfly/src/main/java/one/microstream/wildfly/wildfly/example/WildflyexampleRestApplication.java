6327761c33d8e9989dd98144d29110abfdc85222
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-13 17:22:58 +0000

    Add wildfly as module
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ wildfly/src/main/java/one/microstream/wildfly/wildfly/example/WildflyexampleRestApplication.java
@@ -0,0 +1,11 @@
+package one.microstream.wildfly.wildfly.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class WildflyexampleRestApplication extends Application {
+}

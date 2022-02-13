27e111208f0eea1df4ba6b829529e9f211a1a5b7
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-13 17:51:04 +0000

    Add liberty sample code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ liberty/src/main/java/one/microstream/liberty/liberty/example/LibertyexampleRestApplication.java
@@ -0,0 +1,11 @@
+package one.microstream.liberty.liberty.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class LibertyexampleRestApplication extends Application {
+}

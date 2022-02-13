d0cf7c848d809631e095d0656a346131209407af
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-13 17:48:26 +0000

    Update product controller
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- payara/src/main/java/one/microstream/payara/ProductController.java
+++ payara/src/main/java/one/microstream/payara/ProductController.java
@@ -25,11 +25,13 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -37,20 +39,12 @@ import java.util.Collection;
 
 @RequestScoped
 @Path("products")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ProductController {
 
-    private ProductRepository repository;
-
-    /**
-     * @Deprecated CDI only
-     */
-    ProductController() {
-    }
-
     @Inject
-    ProductController(ProductRepository repository) {
-        this.repository = repository;
-    }
+    private ProductRepository repository;
 
     //TODO don't worried about pagination
     @GET

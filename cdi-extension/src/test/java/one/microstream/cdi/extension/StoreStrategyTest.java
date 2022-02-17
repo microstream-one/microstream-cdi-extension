e160d618a01b66b63859dd6e1a9962b626c392df
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-17 07:55:59 +0000

    Fix import in StoreStrategyTest
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-extension/src/test/java/one/microstream/cdi/extension/StoreStrategyTest.java
+++ cdi-extension/src/test/java/one/microstream/cdi/extension/StoreStrategyTest.java
@@ -19,8 +19,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StoreStrategyTest {
 
     @Test

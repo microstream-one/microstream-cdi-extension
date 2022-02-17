52d15197a96a1ca17b9b79353149630a1d8d419b
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-17 10:58:38 +0000

    improve the storetype documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-extension/src/main/java/one/microstream/cdi/StoreType.java
+++ cdi-extension/src/main/java/one/microstream/cdi/StoreType.java
@@ -26,16 +26,21 @@ package one.microstream.cdi;
  */
 public enum StoreType {
     /**
+     * Lazy storing is the default storing mode of the MicroStream engine.
+     * Referenced instances are stored only if they have not been stored yet.
+     * If a referenced instance has been stored previously it is not stored again even if it has been modified.
      * It will use the method {@link one.microstream.storage.types.StorageManager#store}
      * E.g.: The list of products: storageManager.store(inventory.getProducts());
      */
     LAZY,
     /**
+     * In eager storing mode referenced instances are stored even if they had been stored before.
+     * Contrary to Lazy storing this will also store modified child objects at the cost of performance.
      * It will {@link one.microstream.storage.types.StorageManager#createEagerStorer}.
      * E.g.:
-     *  Storer storer = storage.createEagerStorer();
-     *  storer.store(inventory.getProducts());
-     *  storer.commit();
+     * Storer storer = storage.createEagerStorer();
+     * storer.store(inventory.getProducts());
+     * storer.commit();
      */
     EAGER;
 }
